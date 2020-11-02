c704744e61a545b5892f81705d71329b97f167cd
Corey Martin <coreyjmartin2@gmail.com>
2020-11-02 01:54:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,10 +25,11 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students 
-      WHERE grade = ?
+      WHERE grade = 9 
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
+      
     
   end
 
