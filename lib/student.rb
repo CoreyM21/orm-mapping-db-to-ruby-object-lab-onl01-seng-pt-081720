1d3161cc1998a5a05afb7bb582936f0838ddc574
Corey Martin <coreyjmartin2@gmail.com>
2020-11-02 01:53:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,16 @@ class Student
     SQL
     
     DB[:conn].execute(sql)
+  end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
     
   end
 
